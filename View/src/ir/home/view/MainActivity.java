de5f8e19a12554c8ae40d24edc183f1f0aa4e3f2
ahmadi <karim.medusa@gmail.com>
2014-05-26 13:26:50 +0430

    create android projects

---
+++ View/src/ir/home/view/MainActivity.java
@@ -0,0 +1,14 @@
+package ir.home.view;
+
+import ir.home.habbeh.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);        
+    }
+}
