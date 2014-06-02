8ca89c05bc7a2e43a1ffcef02de4088c5b03f28d
ahmadi <karim_medusa@yahoo.com>
2014-06-02 14:44:44 +0430

    use controller in view

--- View/src/ir/home/view/MainActivity.java
+++ View/src/ir/home/view/MainActivity.java
@@ -1,5 +1,10 @@
 package ir.home.view;
 
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import ir.home.controller.UserController;
 import ir.home.habbeh.R;
 import android.app.Activity;
 import android.os.Bundle;
@@ -9,6 +14,16 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);           
+        setContentView(R.layout.main);  
+        
+        UserController controller=new UserController();
+        try {
+			controller.register("test", "test@yahoo.com", "123");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (XmlPullParserException e) {
+			e.printStackTrace();
+		}
+        
     }
 }
