4d5d2f8f54057fd9373d89cb05c8dbabc9f1d6f0
roomezonline <roomezonline@gmail.com>
2014-06-08 13:37:26 -0700

    Edit MainActivity,Add OnlineMessage,add OfflineMessage,add Findpeople,add UsContact,add UsAbout,add HabbehAbout

--- View/src/ir/home/view/MainActivity.java
+++ View/src/ir/home/view/MainActivity.java
@@ -1,29 +1,116 @@
 package ir.home.view;
 
-import java.io.IOException;
-
-import org.xmlpull.v1.XmlPullParserException;
-
-import ir.home.controller.UserController;
 import ir.home.habbeh.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+   
+	private Button Login;
+	private Button Register;
+	private Button OnlineMessage;
+	private Button OfflineMessage;
+	private Button Findpeople;
+	private Button UsContact;
+	private Button UsAbout;
+	private Button HabbehAbout;
+
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);  
         
-        UserController controller=new UserController();
-        try {
-			controller.register("test", "test@yahoo.com", "123");
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (XmlPullParserException e) {
-			e.printStackTrace();
-		}
+    Login=(Button)findViewById(R.id.Login);
+      Login.setOnClickListener(new OnClickListener() {
+        	
+        public void onClick(View view){
+			Intent myIntent = new Intent(view.getContext(), UserLogin.class);
+            startActivityForResult(myIntent, 0);	
+        }
+      }
+    );
+        
+     
+      Register=(Button)findViewById(R.id.Register);
+      Register.setOnClickListener(new OnClickListener() {
+        	
+        public void onClick(View view){
+			Intent myIntent = new Intent(view.getContext(), UserRegister.class);
+            startActivityForResult(myIntent, 0);	
+        }
+      }
+    );
+      
+      
+      OnlineMessage=(Button)findViewById(R.id.OnlineMessage);
+      OnlineMessage.setOnClickListener(new OnClickListener() {
+        	
+        public void onClick(View view){
+			Intent myIntent = new Intent(view.getContext(), OnlineTextMessage.class);
+            startActivityForResult(myIntent, 0);	
+        }
+      }
+    );
+      
+      
+      OfflineMessage=(Button)findViewById(R.id.OfflineMessage);
+      OfflineMessage.setOnClickListener(new OnClickListener() {
+        	
+        public void onClick(View view){
+			Intent myIntent = new Intent(view.getContext(), OfflineTextMessage.class);
+            startActivityForResult(myIntent, 0);	
+        }
+      }
+    );
+      
+      
+      Findpeople=(Button)findViewById(R.id.Findpeople);
+      Findpeople.setOnClickListener(new OnClickListener() {
+        	
+        public void onClick(View view){
+			Intent myIntent = new Intent(view.getContext(), Findpeople.class);
+            startActivityForResult(myIntent, 0);	
+        }
+      }
+    );
+      
+      
+      UsContact=(Button)findViewById(R.id.UsContact);
+      UsContact.setOnClickListener(new OnClickListener() {
+        	
+        public void onClick(View view){
+			Intent myIntent = new Intent(view.getContext(), UsContact.class);
+            startActivityForResult(myIntent, 0);	
+        }
+      }
+    );
+      
+      
+      UsAbout=(Button)findViewById(R.id.UsAbout);
+      UsAbout.setOnClickListener(new OnClickListener() {
+        	
+        public void onClick(View view){
+			Intent myIntent = new Intent(view.getContext(), UsAbout.class);
+            startActivityForResult(myIntent, 0);	
+        }
+      }
+    );
+      
+      
+      HabbehAbout=(Button)findViewById(R.id.HabbehAbout);
+      HabbehAbout.setOnClickListener(new OnClickListener() {
+        	
+        public void onClick(View view){
+			Intent myIntent = new Intent(view.getContext(), HabbehAbout.class);
+            startActivityForResult(myIntent, 0);	
+        }
+      }
+    );
         
     }
 }
---
+++ View/src/ir/home/view/UsContact.java
@@ -0,0 +1,18 @@
+package ir.home.view;
+
+import ir.home.habbeh.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UsContact extends Activity {
+
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.uscontact);
+
+	}
+
+}
